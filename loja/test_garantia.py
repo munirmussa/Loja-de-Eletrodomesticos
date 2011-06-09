e1c96fc9e1e7b13b415310f4e3efd3c018b64376
Saulo Chaves <saulo@saulo-Oro-PC.(none)>
2011-06-09 11:33:09 -0300

    Comment

---
+++ loja/test_garantia.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+import unittest
+from should_dsl import should
+from garantia import Garantia
+
+class TestGarantia(unittest.TestCase):
+
+    def test_definir_garantia(self):
+        pass
+
+if __name__=="__main__":
+    unittest.main()
