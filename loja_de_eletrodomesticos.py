027f6fdd9ec3fa632c30a5713ddb89f8f19bf729
munirmussa <munirmussa@gmail.com>
2011-06-08 21:30:33 -0300

    Alterando a estrutura dos arquivos

--- loja_de_eletrodomesticos.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-from datetime import date
-
-class Aparelho():
-    
-    def __init__(self, marca, modelo, numero_de_serie):
-        self.marca = marca
-        self.modelo = modelo
-        self.numero_de_serie = numero_de_serie
