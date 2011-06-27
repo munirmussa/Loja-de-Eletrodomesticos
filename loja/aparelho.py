56d2e69a568c1ee357035222cff7d939458680ec
munirmussa <munirmussa@gmail.com>
2011-06-27 10:23:09 -0300

    alterados metodos da classe aparelho

--- loja/aparelho.py
+++ loja/aparelho.py
@@ -3,11 +3,25 @@ from garantia import Garantia
 
 class Aparelho():
     
-    numero_de_serie = 0
+    numero_de_serie = 1
+    aparelhos = []
 
     def __init__(self, marca, modelo, quantidade):
         self.marca = marca
         self.modelo = modelo
-        self.quantidade = quantidade
-        numero_de_serie += 1
+        self.numero_de_serie = Aparelho.numero_de_serie
+        for aparelho in (0, quantidade):   
+            Aparelho(self.marca,self.modelo,1)
+            self.estoque(self)
+        
+        numero_de_serie += 1 
+
+    def estoque(self):
+        self.aparelhos.append(self)
+
+    def verificar_quantidade(self):
+        resultado = ""
+        for x in range(0, len(Storage.aparelhos)):
+            resultado = resultado + Aparelho.aparelhos[x].marca + Aparelho.aparelhos[x].modelo + Aparelho.aparelhos[x].numero_de_serie))
+        return resultado
 
