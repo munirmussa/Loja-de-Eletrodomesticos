da8c836e9af8da05ab59f296a9c6dc0fff0f21dc
munirmussa <munirmussa@gmail.com>
2011-07-07 01:07:13 -0300

    alterada classe cliente

--- loja/cliente.py
+++ loja/cliente.py
@@ -5,7 +5,3 @@ class Cliente():
 	def __init__(self, nome, cidade, bairro, rua, numero):
 		self.nome = nome
 		self.endereco = {'cidade':cidade,'bairro':bairro,'rua':rua,'numero':numero}
-#        self.cidade = cidade
-#        self.bairro = bairro
-#        self.rua = rua
-#        self.numero = numero
